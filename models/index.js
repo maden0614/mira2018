1727fdca183f528820ed7e51e13503e6676f1e61
alvaz57 <alondravazquez2575@gmail.com>
2018-08-11 15:13:51 -0700

    Created a lot of stuff....

---
+++ models/index.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var BookSchema = new Schema({
+  // address: 'Some Address',
+  // zipCode: '95050',
+  // date: { "date": new Date() },
+  // by: 'anonymous',
+  // tags: ['area1', 'area2']
+
+});
